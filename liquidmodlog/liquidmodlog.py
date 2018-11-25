cb89605fe2563b3fbd379f98b9362a9ea34b2885
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-11-25 18:02:07 +0100

    Update liquidmodlog.py

--- liquidmodlog/liquidmodlog.py
+++ liquidmodlog/liquidmodlog.py
@@ -1,1 +1,29 @@
 import discord
+from discord.ext import commands
+from .utils.dataIO import fileIO
+from .utils import checks
+from __main__ import send_cmd_help
+import random
+from random import choice
+from copy import deepcopy
+from random import randint
+from random import choice as randchoice
+import base64
+import json
+import aiohttp
+import itertools
+
+class liquidmodlog:
+    """liquidmodlog"""
+    
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(pass_context = True)
+    async def testingwork(self, ctx):
+        await self.bot.say('Yeet')
+        
+        
+def setup(bot):
+    n = liquidmodlog(bot)
+    bot.add_cog(n)
