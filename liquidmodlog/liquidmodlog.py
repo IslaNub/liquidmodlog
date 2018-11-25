2e5062aa537f2572ceeca56f3f306c10cd6f5ea4
IslaNub <34550190+IslaNub@users.noreply.github.com>
2018-11-25 20:22:51 +0100

    Update liquidmodlog.py

--- liquidmodlog/liquidmodlog.py
+++ liquidmodlog/liquidmodlog.py
@@ -607,7 +607,7 @@ class liquidmodlog:
                 old = ''.join([r.name for r in before.roles])
                 new = ''.join([r.name for r in after.roles])
                 name = " ~ ".join((name.name, name.nick)) if name.nick else name.name
-                role = discord.Embed(description=name, colour=discord.Color.red())
+                role = discord.Embed(description=name, colour=discord.Color.grey())
                 infomessage = "Update to __{}__'s roles.".format(before.nick if before.nick else before.name)
                 role.add_field(name="Info:", value=infomessage, inline=False)
                 role.add_field(name="Old roles:", value=old, inline=False)
